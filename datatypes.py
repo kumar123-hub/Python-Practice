71f04763b8ee95e8efe8d664ff57fbeccd4c374b
kumar123-hub <chandrakumart948@gmail.com>
2026-07-17 16:07:44 +0530

    Initial commit

---
+++ datatypes.py
@@ -0,0 +1,21 @@
+# string data types
+# name='chandra kumar'
+# print(type(name))
+
+          #numeric data types
+# a=10
+# age=30
+# print(type(a))
+# print(type(age))
+
+# a=10.5
+# salary=1000.50
+# print(type(a))
+# print(type(salary))
+
+# a=10+5j
+# print(type(a))
+
+         # string data types
+name='chandra kumar'
+print(type(name))
